83a50537e501e4db49f5eadf9d8f5f5f9a6eec1e
Dario Breitenstein <online@imakethings.ch>
2019-03-19 14:48:22 +0100

    add submodules

--- src/index.js
+++ src/index.js
@@ -1,63 +1,43 @@
 var uuidV1 = require('uuid/v1')
+var cards = require('./cards.js')
 
-var app = require('express')()
+var express = require('express')
+var path = require('path')
+var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 const PORT = process.env.PORT || 3000;
 
-const cards = [
-  [
-    "Wenn ich gut Arbeite, will ich auch gut bezahlt werden. Mit einer Ausnahme: _WALTERN_ würde ich auch gratis.",
-    "Die am schwersten verkäufliche Massenware ist in der heutigen Zeit wohl _WALTER_.",
-    "Lebte ich in Österreich, hätte ich bestimmt schon den Orden für _WALTER_ erhalten."
-  ],
-  [
-    "Bei einem Mann schaue ich zuallererst auf sein(e/n) _WALTER_, bei einer Frau aber auf ihr(e/n) _WALTER_.",
-    "Das dänische Sprichwort \"Kønnst meg dålø schørpfler i maåsker brælen\" will sagen: \"_WALTER_\".",
-    "Wenn ich als Lebewesen unter der Erde leben müsste, wäre ich am liebsten _WALTER_."
-  ],
-  [
-    "Wetten, dass ich euch alle zum Lachen bringen kann, indem ich _WALTERE_!?",
-    "Wenn ich ein Brettspiel über mein Liebesleben herausgäbe, hiesse es _WALTER_.",
-    "Ich fühle, dass ich in einem meiner früheren Leben eine Sagengestalt war, nämlich _WALTER_."
-  ],
-  [
-    "_WALTER_ ist eine Krankheit, mit der ich ganz gut leben kann.",
-    "Wenn kleine, grüne Wesen vom Mars vor meinem Haus landeten, ginge ich lächelnd auf sie zu und böte ihnen _WALTER_ zu essen an.",
-    "Die heutigen Modeschöpferinnen arbeiten mit Farbkombinationen, die früher als \"unmöglich\" galten. Mir gefällt besonders _WALTER_."
-  ],
-  [
-    "Der Name \"Pinguin\" stammt _WALTER_",
-    "Ein Kompliment, das von mir stammen könnte: \"Schau, für dich ginge ich durch _WALTER_!\"",
-    "Wenn ich mal auf einer noch unentdeckten Insel strande, könnte ich mir gut vorstellen, dass mich die Eingeborenen bald als Gott/Göttin des/der _WALTER_ verehren."
-  ],
-  [
-    "Willst du jemanden loswerden? Sag ihm/ihr bloss: \"Mit dir ausgehen? Lieber würde ich _WALTER_!\"",
-    "Im guten alten Wilden Westen mussten die Männer nichts als reiten und schiessen, und die Frauen mussten bloss _WALTERN_.",
-    "Ich träume davon, eines Tages werde ein(e) _WALTER_ nach mir benannt."
-  ],
-  [
-    "Eine Persönlichkeit, die ihr wohl alle schätzt, die in meinen Augen aber kaum mehr darstellt als eine farblose Schaufensterpuppe, ist _WALTER_.",
-    "Mitverantwortlich für den versteckten Analphabethismus in unserem Land sind _WALTER_.",
-    "Ich hätte Lust, jetzt auf der Stelle _WALTER_."
-  ],
-  [
-    "An manchen Tagen bringe ich rein gar nichts zustande. Heute zum Beispiel habe ich nicht einmal _WALTER_.",
-    "Frappant, wie _WALTER_ und _WALTER_ sich gleichen!",
-    "Es klingt ein bisschen komisch... aber ich habe gelegentlich _WALTER_ im Eisschrank."
-  ],
-  [
-    "Ich bin allergisch auf _WALTER_.",
-    "Wenn ich graue Regenschirme bewerben müsste, wäre _WALTER_ mein Traum-Model.",
-    "Ich denke daran, ein Bekanntschaftsinserat aufzugeben. Angesichts ihres Zielpublikums scheint mir dafür eine bestimmte Zeitschrift besonders geeignet: _WALTER_."
-  ]
-]
-
 const MAX_PLAYER_COUNT = 8
 
 let clients = []
 let rooms = {}
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, '../walter-host/dist')))
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../walter-host/dist')))
+  app.use('/client', express.static(path.join(__dirname, '../walter-client/dist')))
+  
+  app.get('/client/*', (req, res) => {
+    res.sendfile(path.join(__dirname, '../walter-client/dist/index.html'))
+  })
+  app.get('/', (req, res) => {
+    res.sendfile(path.join(__dirname, '../walter-host/dist/index.html'))
+  })
+}
+
+//build mode
+app.get('/client/*', (req, res) => {
+  res.sendfile(path.join(__dirname, '../walter-client/public/index.html'))
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../walter-host/public/index.html'))
+})
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hi!</h1>')
